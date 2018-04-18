8309070c71a3c6691049213492d8a3598cf41ea6
root <root@debian.minjusticia-ddhh.int>
2018-04-17 20:30:50 -0500

    Merge origin/master
    Conflicts:
    	Saturno/src/main/webapp/index.xhtml

--- Saturno/src/main/java/ec/gob/mj/saturno/controller/VListAssignmentController.java
+++ Saturno/src/main/java/ec/gob/mj/saturno/controller/VListAssignmentController.java
@@ -16,7 +16,9 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.NavigationHandler;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.Query;
@@ -28,16 +30,25 @@ public class VListAssignmentController implements Serializable {
     @EJB
     private VListAssignmentFacadeLocal viewAssignmentEJB;
     private VListAssignment Assignment;
-    private List<VListAssignment>  listAssigments;
-    private Integer flagChecked=0;
+    private List<VListAssignment> listAssigments;
+    private Integer flagChecked = 0;
+    private String testlabel;
 
+    public String getTestlabel() {
+        return testlabel;
+    }
+
+    public void setTestlabel(String testlabel) {
+        this.testlabel = testlabel;
+    }
     
+
     public Integer getFlagChecked() {
         return flagChecked;
     }
 
     /*Secction Getters And Setters*/
-    public void setFlagChecked(Integer flagChecked) {    
+    public void setFlagChecked(Integer flagChecked) {
         this.flagChecked = flagChecked;
     }
 
@@ -64,15 +75,23 @@ public class VListAssignmentController implements Serializable {
     public void setListAssigments(List<VListAssignment> listAssigments) {
         this.listAssigments = listAssigments;
     }
-    
-    
+
     /*Section Construct */
     @PostConstruct
     public void init() {
-      listAssigments=  viewAssignmentEJB.findAll();
-      //listAssigments=viewAssignmentEJB.findListAssignments();
+        
+        listAssigments = viewAssignmentEJB.findAll();
+        //listAssigments=viewAssignmentEJB.findListAssignments();
         //Assignment = new VListAssignment();
     }
 
- 
+    
+    public void doSomething() {
+        System.out.println("Hello word");
+        this.testlabel="hello";
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("test", "test"));
+        // Now, we move to another page...
+
+    }
+
 }
